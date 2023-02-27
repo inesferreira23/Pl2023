3d7a03072f2bff7e60b080d7b679ce9d759dc279
inesferrreira23 <a97040@alunos.uminho.pt>
2023-02-27 15:31:54 +0000

    TPC2 feito

--- TPC2/main.py
+++ TPC2/main.py
@@ -1,3 +1,5 @@
+from sys import stdin
+
 def removeMenos(num):
     fs = ""
     sc = ""
@@ -9,42 +11,40 @@ def removeMenos(num):
     while i < len(num):
         sc = sc + num[i]
         i += 1
-    res = int(fs)-int(sc)
+    res = float(fs)-float(sc)
     return res
+
 
 def main():
     On = True
     count = 0
     num = "0"
-    texto = input("Insira um texto: ")
-    print("\n")
-
-    for i in range(0, len(texto)):
-        if texto[i] != "=":
-            if On:
-                if texto[i].isdigit() or texto[i] == "-" and texto[i+1].isdigit():
-                    num = num + texto[i]
-                else:
-                    if "-" in num: 
-                     aux = removeMenos(num)
-                     count += int(aux)
-                     num = "0"
+    for texto in stdin:
+        for i in range(0, len(texto)):
+            if texto[i] != "=":
+                if On:
+                    if texto[i].isdigit() or texto[i] == "-" and texto[i+1].isdigit() or texto[i] == "." and texto[i+1].isdigit():
+                        num = num + texto[i]
                     else:
-                        count += int(num)
-                        num = "0"
+                        if "-" in num: 
+                            aux = removeMenos(num)
+                            count += float(aux)
+                            num = "0"
+                        else:
+                            count += float(num)
+                            num = "0"
 
-            if texto[i] in "Oo" and texto[i+1] in "Nn":
-                On = True
-            if texto[i] in "Oo" and texto[i+1] in "Ff" and texto[i+2] in "Ff":
-                On = False
-        else:
-            if "-" in num:
-                aux = removeMenos(num) 
-                count += int(aux)
-                num = "0"
+                if texto[i] in "Oo" and texto[i+1] in "Nn":
+                    On = True
+                if texto[i] in "Oo" and texto[i+1] in "Ff" and texto[i+2] in "Ff":
+                    On = False
             else:
-                count += int(num)
-                num = "0"
-            print("A soma dos numeros inseridos é:" + str(count))
-
+                if "-" in num:
+                    aux = removeMenos(num) 
+                    count += float(aux)
+                    num = "0"
+                else:
+                    count += float(num)
+                    num = "0"
+                print("A soma dos numeros inseridos é:" + str(count))
 main()
